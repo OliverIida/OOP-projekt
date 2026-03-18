ce6e957076d637ed3861cd21c60db1d59fe6bddd
Karl-The-Man <karlelmar.v@gmail.com>
2026-03-18 15:35:58 +0200

    Add Kaardipakk class with methods for managing a deck of cards

--- Kaardipakk.java
+++ Kaardipakk.java
@@ -1,3 +1,31 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Kaardipakk {
+    private final List<Kaart> kaardid;
+
+    public Kaardipakk() {
+        this.kaardid = new ArrayList<>();
+    }
+
+    public void lisaKaart(Kaart kaart) {
+        kaardid.add(kaart);
+    }
     
+    public void segaKaardid() {
+        Collections.shuffle(kaardid);
+    }
+
+    public Kaart looKaart() {
+        return kaardid.remove(0);
+    }
+    
+    public int getKaardipakkiSuurus() {
+        return kaardid.size();
+    }
+
+    public String toString() {
+        return kaardid.toString();
+    }
 }
