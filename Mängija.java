86132fbec8bc74da8c411ba4fb54e8051dd05f43
Elias Teikari <elias.tkri@gmail.com>
2026-03-18 15:32:47 +0200

    Algsed muutujad defineeritud Mängija klassis, lisatud mängi() meetod.

--- Mängija.java
+++ Mängija.java
@@ -1,3 +1,24 @@
-class Mängija {
+import java.util.ArrayList;
 
+class Mängija {
+    String nimi;
+    int vanus;
+    int raha;
+    int panus;
+    ArrayList<Kaart> kaardid;
+    boolean bust;
+    boolean blackjack;
+    
+    public Mängija(String nimi, int vanus, int raha) {
+        this.nimi = nimi;
+        this.vanus = vanus;
+        this.raha = raha;
+        this.kaardid = new ArrayList<>();
+        this.bust = false;
+        this.blackjack = false;
+    }
+
+    public void mängi() {
+        System.out.println(nimi + " mängib.");
+    }
 }
